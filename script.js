9d74a3fd1a05e141c280a3ed3daf602eb271b0c6
Anton Everaert <antoneveraert@gmail.com>
2019-04-02 14:39:20 +0200

    hide button

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ nyanImage.src = "nyancat.png";
 nyanImage.id = "idNyanCat";
 nyanImage.width = 150;
 nyanImage.style.position = 'absolute';
-
 var top = 0;
 var bottom = 0;
 var left = 0;
@@ -13,14 +12,15 @@ var right = 0;
 rainButton.addEventListener("click", addBackground);
 rainButton.addEventListener("click", moveCat);
 
-
 function addBackground() {
   document.body.style.backgroundImage = "url('background.png')";
+  /*document.getElementById("NyanButton").style.display = "none";*/
 }
+
+var dogIcon = document.createElement ("img");
+dogIcon.src = "dogicon.png";
 
 function moveCat() {
 	document.body.appendChild(nyanImage);
 	document.getElementById("idNyanCat").style.animation = "catRain 3s";
 }
-
-    
